5a7f010a30b27ed0705d5acff085671921e29feb
João Pedro Areias de Moraes <joao.pedro.a.moraes@accenture.com>
2023-11-09 17:27:45 -0300

    Accept request and returns code 200

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+
+fn handle_stream(mut stream: TcpStream) -> std::io::Result<()>{
+    let mut buffer = vec![0; 255];
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let bytes_read= stream.read(buffer.as_mut_slice());
+
+
+    match bytes_read {
+        Ok(_) =>{
+            println!("{}", String::from_utf8(buffer).unwrap());
+            let _ = stream.write(response.as_bytes());
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    }
+    return Ok(())
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,6 +29,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                let _ = handle_stream(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
